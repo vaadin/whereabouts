b9fb4e36e91b98952797d0b66952cc80cbdedd4a
Petter Holmstrom <petter@vaadin.com>
2025-07-01 08:37:17 +0300

    Add missing package-info.java

---
+++ src/main/java/com/example/application/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.example.application.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/com/example/application/taskmanagement/domain/event/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.example.application.taskmanagement.domain.event;
+
+import org.jspecify.annotations.NullMarked;
