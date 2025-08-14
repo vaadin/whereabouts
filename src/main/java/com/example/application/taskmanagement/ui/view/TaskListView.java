a757a93554ad4f77447a0cced773001935e33a58
Petter Holmstrom <petter@vaadin.com>
2025-08-14 16:20:26 +0300

    Simplify TaskListView

--- src/main/java/com/example/application/taskmanagement/ui/view/TaskListView.java
+++ src/main/java/com/example/application/taskmanagement/ui/view/TaskListView.java
@@ -29,6 +29,7 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.menubar.MenuBarVariant;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -42,6 +43,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.time.format.TextStyle;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -54,7 +56,6 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
     private final AppUserInfoLookup appUserInfoLookup;
     private final TaskService taskService;
     private final H2 title;
-    private final ConditionalComponent<Boolean> gridWrapper;
     private final TaskList taskList;
     private final ZoneId timeZone;
     private final boolean isAdmin;
@@ -63,7 +64,7 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
     private Project project;
 
     TaskListView(AuthenticationContext authenticationContext, CurrentUser currentUser,
-            AppUserInfoLookup appUserInfoLookup, TaskService taskService) {
+                 AppUserInfoLookup appUserInfoLookup, TaskService taskService) {
         isAdmin = authenticationContext.hasRole(AppRoles.ADMIN);
 
         this.appUserInfoLookup = appUserInfoLookup;
@@ -77,11 +78,9 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
 
         taskList = new TaskList();
 
-        gridWrapper = ConditionalComponent.createBinary(taskList, new NoTasks(), false);
-
         setSizeFull();
         addClassNames("task-list-view", LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN);
-        add(new SectionToolbar(title, addTaskButton), gridWrapper);
+        add(new SectionToolbar(title, addTaskButton), taskList);
     }
 
     @Override
@@ -97,30 +96,74 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
 
     @Override
     public String getPageTitle() {
-        return "Tasks - " + Optional.ofNullable(project).map(Project::getName).orElse("No Project");
+        return "Project Tasks - " + Optional.ofNullable(project).map(Project::getName).orElse("No Project");
     }
 
-    private class NoTasks extends Div {
-
-        // TODO Would be awesome if this could be used as the empty content of the grid, but then it would also show up
-        //  when the grid is empty because of a filter, not because there are no tasks.
-
-        NoTasks() {
-            var icon = new SvgIcon("icons/list_alt_check.svg");
-            icon.setSize("60px");
-            var title = new H4("No tasks yet");
-            var instruction = new Span("Add a task to get started");
-            var addProject = new Button("Add Task", VaadinIcon.PLUS.create(), event -> addTask());
-            addProject.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-
-            setSizeFull();
-            addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN, LumoUtility.AlignItems.CENTER,
-                    LumoUtility.JustifyContent.CENTER);
-            var centerDiv = new Div(icon, title, instruction, addProject);
-            centerDiv.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
-                    LumoUtility.AlignItems.CENTER, LumoUtility.Gap.SMALL);
-            add(centerDiv);
+    private void addTask() {
+        if (project == null) {
+            throw new IllegalStateException("Cannot add task: project is null");
         }
+
+        var dialog = new AddTaskDialog(appUserInfoLookup, () -> taskService.createTask(project), newTask -> {
+            taskService.saveTask(newTask);
+            refresh();
+            notifyProjectTasksChanged();
+            Notifications.createNonCriticalNotification(new SvgIcon("icons/check.svg"), "Task created successfully",
+                    NotificationVariant.LUMO_SUCCESS).open();
+        });
+        dialog.open();
+    }
+
+    private void editTask(Task task) {
+        var dialog = new EditTaskDialog(appUserInfoLookup, task, editedTask -> {
+            refreshTask(taskService.saveTask(editedTask));
+            notifyProjectTasksChanged();
+            Notifications.createNonCriticalNotification(new SvgIcon("icons/check.svg"), "Task updated successfully",
+                    NotificationVariant.LUMO_SUCCESS).open();
+        });
+        dialog.open();
+    }
+
+    private void deleteTask(Task task) {
+        var dialog = new ConfirmDialog("Delete Task", "Are you sure you want to delete this task?", "Delete", event -> {
+            taskService.deleteTask(task);
+            refresh();
+            notifyProjectTasksChanged();
+            Notifications.createNonCriticalNotification(new SvgIcon("icons/delete_sweep.svg"),
+                    "Task deleted successfully", NotificationVariant.LUMO_ERROR).open();
+        }, "Cancel", event -> {
+        });
+        dialog.setConfirmButtonTheme("error primary");
+        dialog.open();
+    }
+
+    private void refresh() {
+        if (project == null) {
+            throw new IllegalStateException("Cannot refresh: project is null");
+        }
+        title.setText(project.getName());
+        taskList.grid.getDataProvider().refreshAll();
+    }
+
+    private void refreshTask(Task task) {
+        taskList.grid.getDataProvider().refreshItem(task);
+    }
+
+    private void notifyProjectTasksChanged() {
+        if (project == null) {
+            throw new IllegalStateException("Cannot notify: project is null");
+        }
+        var event = new ProjectTasksChangedEvent(this, project.requireId());
+        propagateEvent(this, event);
+    }
+
+    private static void propagateEvent(Component component, ComponentEvent<? extends Component> event) {
+        ComponentUtil.fireEvent(component, event);
+        component.getParent().ifPresent(parent -> propagateEvent(parent, event));
+    }
+
+    public static void showTasksForProjectId(Long projectId) {
+        UI.getCurrent().navigate(TaskListView.class, new RouteParam(PARAM_PROJECT_ID, projectId));
     }
 
     private class TaskList extends Div {
@@ -129,10 +172,9 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
                 .withLocale(getLocale());
         private final DateTimeFormatter timeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
                 .withLocale(getLocale());
-        private final Grid<Task> wideGrid;
-        private final Grid<Task> narrowGrid;
-        private final ConditionalComponent<Component> grid;
+        private final Grid<Task> grid;
         private final TaskFilter filter;
+        private final Grid.Column<Task> cardColumn;
 
         TaskList() {
             filter = new TaskFilter();
@@ -149,47 +191,53 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
 
             var filterMenu = createFilterMenu();
 
-            wideGrid = new Grid<>();
-            wideGrid.setSelectionMode(Grid.SelectionMode.NONE);
-            wideGrid.setItemsPageable(pageable -> taskService.findTasks(project, filter, pageable)); // TODO project can be null. What to do about it?
-            wideGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-            wideGrid.addColumn(new ComponentRenderer<>(this::createStatusBadge)).setHeader("Status").setWidth("150px")
+            grid = new Grid<>();
+            grid.setSelectionMode(Grid.SelectionMode.NONE);
+            grid.setItemsPageable(pageable -> project == null ? Collections.emptyList() : taskService.findTasks(project, filter, pageable));
+            grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+            grid.addColumn(new ComponentRenderer<>(this::createStatusBadge)).setHeader("Status").setWidth("150px")
                     .setFlexGrow(0).setSortProperty(Task.STATUS_SORT_PROPERTY);
-            wideGrid.addColumn(Task::getDescription).setHeader("Description").setFlexGrow(1)
+            grid.addColumn(Task::getDescription).setHeader("Description").setFlexGrow(1)
                     .setSortProperty(Task.DESCRIPTION_SORT_PROPERTY);
-            wideGrid.addColumn(new ComponentRenderer<>(this::createDueDate))
+            grid.addColumn(new ComponentRenderer<>(this::createDueDate))
                     .setHeader("Due Date (%s)".formatted(timeZone.getDisplayName(TextStyle.SHORT, getLocale())))
                     .setWidth("200px").setFlexGrow(0).setSortProperty(Task.DUE_DATE_SORT_PROPERTY);
-            wideGrid.addColumn(new ComponentRenderer<>(this::createPriorityBadge)).setHeader("Priority").setWidth("150px")
+            grid.addColumn(new ComponentRenderer<>(this::createPriorityBadge)).setHeader("Priority").setWidth("150px")
                     .setFlexGrow(0).setSortProperty(Task.PRIORITY_SORT_PROPERTY);
-            wideGrid.addColumn(new ComponentRenderer<>(this::createAssignees)).setHeader("Assignees");
-            wideGrid.addColumn(new ComponentRenderer<>(this::createActionMenu)).setTextAlign(ColumnTextAlign.END)
+            grid.addColumn(new ComponentRenderer<>(this::createAssignees)).setHeader("Assignees");
+            grid.addColumn(new ComponentRenderer<>(this::createActionMenu)).setTextAlign(ColumnTextAlign.END)
                     .setWidth("60px").setFlexGrow(0);
-            createContextMenu(wideGrid.addContextMenu());
-
-            narrowGrid = new Grid<>();
-            narrowGrid.setSelectionMode(Grid.SelectionMode.NONE);
-            narrowGrid.setDataProvider(wideGrid.getDataProvider());
-            narrowGrid.getLazyDataView().setItemCountUnknown();
-            narrowGrid.addColumn(new ComponentRenderer<>(this::createTaskCard));
-            createContextMenu(narrowGrid.addContextMenu());
-
-            grid = ConditionalComponent.createBistate(wideGrid, narrowGrid, wideGrid);
+            cardColumn = grid.addColumn(new ComponentRenderer<>(this::createTaskCard));
+            grid.setEmptyStateComponent(new TaskListEmptyComponent());
+            createContextMenu(grid.addContextMenu());
 
             setSizeFull();
             addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN);
             add(new SectionToolbar(searchField, filterMenu), grid);
 
             var resizeObserver = new ResizeObserver(this);
-            ProgressiveConditionalComponentSwitcher.switchStateOnResize(grid, resizeObserver, ((width, height) -> width < 800 ? narrowGrid : wideGrid));
+            resizeObserver.addListener(this::adjustGridOnResize);
+        }
+
+        private void adjustGridOnResize(ResizeObserver.ResizeEvent resizeEvent) {
+            boolean showCardView = resizeEvent.width() < 800;
+            cardColumn.setVisible(showCardView);
+            grid.getColumns().stream().filter(c -> c != cardColumn).forEach(c -> c.setVisible(!showCardView));
+            if (showCardView) {
+                grid.addClassName("card-view");
+                grid.addThemeVariants(GridVariant.LUMO_NO_ROW_BORDERS);
+            } else {
+                grid.removeClassName("card-view");
+                grid.removeThemeVariants(GridVariant.LUMO_NO_ROW_BORDERS);
+            }
         }
 
         private Component createStatusBadge(Task task) {
             return switch (task.getStatus()) {
-            case PENDING -> Badges.createContrast(task.getStatus().getDisplayName());
-            case PLANNED, IN_PROGRESS -> Badges.createDefault(task.getStatus().getDisplayName());
-            case PAUSED -> Badges.createError(task.getStatus().getDisplayName());
-            case DONE -> Badges.createSuccess(task.getStatus().getDisplayName());
+                case PENDING -> Badges.createContrast(task.getStatus().getDisplayName());
+                case PLANNED, IN_PROGRESS -> Badges.createDefault(task.getStatus().getDisplayName());
+                case PAUSED -> Badges.createError(task.getStatus().getDisplayName());
+                case DONE -> Badges.createSuccess(task.getStatus().getDisplayName());
             };
         }
 
@@ -198,18 +246,18 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
             var themeList = badge.getElement().getThemeList();
             themeList.add("badge");
             switch (task.getPriority()) {
-            case URGENT -> {
-                themeList.add("error");
-            }
-            case HIGH -> {
-                themeList.add("warning");
-            }
-            case NORMAL -> {
-                // Default style
-            }
-            case LOW -> {
-                themeList.add("success");
-            }
+                case URGENT -> {
+                    themeList.add("error");
+                }
+                case HIGH -> {
+                    themeList.add("warning");
+                }
+                case NORMAL -> {
+                    // Default style
+                }
+                case LOW -> {
+                    themeList.add("success");
+                }
             }
             return badge;
         }
@@ -316,71 +364,23 @@ class TaskListView extends Main implements AfterNavigationObserver, HasDynamicTi
         }
     }
 
-    private void addTask() {
-        if (project == null) {
-            throw new IllegalStateException("Cannot add task: project is null");
+    private class TaskListEmptyComponent extends VerticalLayout {
+        TaskListEmptyComponent() {
+            var icon = new SvgIcon("icons/list_alt_check.svg");
+            icon.setSize("60px");
+            var title = new H4("No tasks found");
+            var instruction = new Span("Change the search criteria or add a task");
+
+            var addTask = new Button("Add Task", VaadinIcon.PLUS.create(), event -> addTask());
+            addTask.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+            // TODO Add "Clear search criteria" button
+
+            add(icon, title, instruction, addTask);
+
+            setSizeFull();
+            setAlignItems(Alignment.CENTER);
+            setJustifyContentMode(JustifyContentMode.CENTER);
         }
-
-        var dialog = new AddTaskDialog(appUserInfoLookup, () -> taskService.createTask(project), newTask -> {
-            taskService.saveTask(newTask);
-            refresh();
-            notifyProjectTasksChanged();
-            Notifications.createNonCriticalNotification(new SvgIcon("icons/check.svg"), "Task created successfully",
-                    NotificationVariant.LUMO_SUCCESS).open();
-        });
-        dialog.open();
-    }
-
-    private void editTask(Task task) {
-        var dialog = new EditTaskDialog(appUserInfoLookup, task, editedTask -> {
-            refreshTask(taskService.saveTask(editedTask));
-            notifyProjectTasksChanged();
-            Notifications.createNonCriticalNotification(new SvgIcon("icons/check.svg"), "Task updated successfully",
-                    NotificationVariant.LUMO_SUCCESS).open();
-        });
-        dialog.open();
-    }
-
-    private void deleteTask(Task task) {
-        var dialog = new ConfirmDialog("Delete Task", "Are you sure you want to delete this task?", "Delete", event -> {
-            taskService.deleteTask(task);
-            refresh();
-            notifyProjectTasksChanged();
-            Notifications.createNonCriticalNotification(new SvgIcon("icons/delete_sweep.svg"),
-                    "Task deleted successfully", NotificationVariant.LUMO_ERROR).open();
-        }, "Cancel", event -> {
-        });
-        dialog.setConfirmButtonTheme("error primary");
-        dialog.open();
-    }
-
-    private void refresh() {
-        if (project == null) {
-            throw new IllegalStateException("Cannot refresh: project is null");
-        }
-        title.setText(project.getName());
-        gridWrapper.setState(taskService.hasTasks(project));
-        taskList.wideGrid.getDataProvider().refreshAll();
-    }
-
-    private void refreshTask(Task task) {
-        taskList.wideGrid.getDataProvider().refreshItem(task);
-    }
-
-    private void notifyProjectTasksChanged() {
-        if (project == null) {
-            throw new IllegalStateException("Cannot notify: project is null");
-        }
-        var event = new ProjectTasksChangedEvent(this, project.requireId());
-        propagateEvent(this, event);
-    }
-
-    private static void propagateEvent(Component component, ComponentEvent<? extends Component> event) {
-        ComponentUtil.fireEvent(component, event);
-        component.getParent().ifPresent(parent -> propagateEvent(parent, event));
-    }
-
-    public static void showTasksForProjectId(Long projectId) {
-        UI.getCurrent().navigate(TaskListView.class, new RouteParam(PARAM_PROJECT_ID, projectId));
     }
 }
