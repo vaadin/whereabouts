a3c417812d0d31d019c1bc506586d8b1a12c7d30
Petter Holmstrom <petter@vaadin.com>
2025-09-25 09:53:52 +0300

    Add USPostalAddress

---
+++ src/main/java/com/example/application/common/address/usa/USPostalAddress.java
@@ -0,0 +1,22 @@
+package com.example.application.common.address.usa;
+
+import com.example.application.common.Country;
+import com.example.application.common.address.PostalAddress;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+public record USPostalAddress(@Nullable String streetAddress, @Nullable String city, @Nullable USState state,
+                              @Nullable USZipCode zipCode, Country country) implements PostalAddress {
+
+    static final int MAX_STRING_LENGTH = 150;
+
+    public USPostalAddress {
+        if (streetAddress != null && streetAddress.length() > MAX_STRING_LENGTH) {
+            throw new IllegalArgumentException("streetAddress is too long");
+        }
+        if (city != null && city.length() > MAX_STRING_LENGTH) {
+            throw new IllegalArgumentException("city is too long");
+        }
+    }
+}
