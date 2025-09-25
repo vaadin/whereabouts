5a4e3233fc07db6730871f389f03dce473e60e67
Petter Holmstrom <petter@vaadin.com>
2025-09-25 10:32:37 +0300

    Add GermanPostalAddress

---
+++ src/main/java/com/example/application/common/address/germany/GermanPostalAddress.java
@@ -0,0 +1,22 @@
+package com.example.application.common.address.germany;
+
+import com.example.application.common.Country;
+import com.example.application.common.address.PostalAddress;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+@NullMarked
+public record GermanPostalAddress(@Nullable String streetAddress, @Nullable GermanPostalCode postalCode,
+                                  @Nullable String city, Country country) implements PostalAddress {
+
+    public static final int MAX_STRING_LENGTH = 150;
+
+    public GermanPostalAddress {
+        if (streetAddress != null && streetAddress.length() > MAX_STRING_LENGTH) {
+            throw new IllegalArgumentException("streetAddress is too long");
+        }
+        if (city != null && city.length() > MAX_STRING_LENGTH) {
+            throw new IllegalArgumentException("city is too long");
+        }
+    }
+}
