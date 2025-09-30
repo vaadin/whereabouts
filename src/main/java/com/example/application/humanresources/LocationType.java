59d8cc0125320fa46fc145de204cf3fc0d761fe2
Petter Holmstrom <petter@vaadin.com>
2025-09-30 12:03:28 +0300

    LocationService + first data types

---
+++ src/main/java/com/example/application/humanresources/LocationType.java
@@ -0,0 +1,18 @@
+package com.example.application.humanresources;
+
+public enum LocationType {
+    REGIONAL_HQ("Regional headquarters"),
+    BRANCH_OFFICE("Branch office"),
+    REMOTE_HUB("Remote working hub"),
+    GLOBAL_HQ("Global headquarters");
+
+    private final String displayName;
+
+    LocationType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String displayName() {
+        return displayName;
+    }
+}
